d37a1a65d289b9764e7dee3641e5ada9eb6869d5
Prayansh Srivastava <prayansh@gmail.com>
2020-03-10 02:16:50 -0700

    fully testing structs for users

--- routes/user_test.go
+++ routes/user_test.go
@@ -8,6 +8,35 @@ import (
 	"testing"
 )
 
+var UserBarry = &model.User{
+	ID:         "e99a988a-1d41-3997-8d59-959a48ac24a0",
+	Name:       "Barry Allen",
+	Department: "R&D",
+	IsAdmin:    false,
+	Email:      "",
+}
+var UserBruce = &model.User{
+	ID:         "8b5bb736-6a1d-3378-8e71-ab45fe8beb84",
+	Name:       "Bruce Wayne",
+	Department: "Engineering",
+	IsAdmin:    true,
+	Email:      "",
+}
+var UserClark = &model.User{
+	ID:         "32ea2fb1-7124-304a-b9c3-eb445578103e",
+	Name:       "Clark Kent",
+	Department: "Marketing",
+	IsAdmin:    false,
+	Email:      "",
+}
+var UserDiana = &model.User{
+	ID:         "ab6c2c4f-c112-3c1e-bcf1-42cdea289c1f",
+	Name:       "Diana Prince",
+	Department: "Operations",
+	IsAdmin:    false,
+	Email:      "",
+}
+
 // main entry point for user endpoints' tests
 func testUsersEndpoints(t *testing.T, app *App) {
 	testGetAllUsers(t, app)
@@ -33,13 +62,14 @@ func testGetOneUserFail(t *testing.T, app *App) {
 }
 
 func testGetOneUser(t *testing.T, app *App) {
+	userId := UserBarry.ID
 	rr := executeReq(t, &testRouteConfig{
 		Method:  http.MethodGet,
 		Body:    nil,
 		Handler: app.GetOneUser,
-		URL:     fmt.Sprintf("/users/%s", "e99a988a-1d41-3997-8d59-959a48ac24a0"),
+		URL:     fmt.Sprintf("/users/%s", userId),
 		URLParams: map[string]string{
-			"id": "e99a988a-1d41-3997-8d59-959a48ac24a0",
+			"id": userId,
 		},
 	})
 
@@ -49,7 +79,7 @@ func testGetOneUser(t *testing.T, app *App) {
 	}
 	var payload *model.User
 	_ = json.Unmarshal(rr.Body.Bytes(), &payload)
-	if !payload.IsAdmin && payload.Name != "Barry Allen" {
+	if *payload != *UserBarry {
 		t.Fatalf("testGetOneUser: incorrect user : got %s", payload.Name)
 	}
 }
@@ -62,33 +92,26 @@ func testGetAllUsers(t *testing.T, app *App) {
 		URL:     "/users/",
 	})
 
-	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
 		t.Fatalf("testGetAllUsers: handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 
-	// Check the response body is what we expect.
 	var payload []*model.User
 	_ = json.Unmarshal(rr.Body.Bytes(), &payload)
 	if len(payload) != 4 {
 		t.Fatalf("testGetAllUsers: incorrect number of users: got %d users", len(payload))
 	}
-	expectedUsers := []string{
-		"Barry Allen",
-		"Bruce Wayne",
-		"Clark Kent",
-		"Diana Prince",
-	}
-	for _, uName := range expectedUsers {
+	expectedUsers := []*model.User{UserBarry, UserBruce, UserClark, UserDiana}
+	for _, expected := range expectedUsers {
 		found := false
 		for _, u := range payload {
-			if u.Name == uName {
+			if *expected == *u {
 				found = true
 			}
 		}
 		if !found {
-			t.Fatalf("testGetAllUsers: %s not found in user list", uName)
+			t.Fatalf("testGetAllUsers: %s not found in user list", expected.Name)
 		}
 	}
 }
