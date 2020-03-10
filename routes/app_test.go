9b0ff7e3870f0a3ccb0e8a281931c12376aeeb57
Prayansh Srivastava <prayansh@gmail.com>
2020-03-09 21:44:26 -0700

    add user endpoints' tests

--- routes/app_test.go
+++ routes/app_test.go
@@ -4,10 +4,33 @@ import (
 	"github.com/gorilla/mux"
 	"go-api/db"
 	"go-api/db/postgres"
+	"io"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
+
+type testRouteConfig struct {
+	Method  string
+	URL     string
+	Path    string
+	Body    io.Reader
+	Handler func(http.ResponseWriter, *http.Request)
+}
+
+func doRequest(t *testing.T, config *testRouteConfig) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(config.Method, config.URL, config.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc(config.Path, config.Handler)
+	router.ServeHTTP(rr, req)
+	return rr
+}
 
 func NewTestApp() *App {
 	dbUrl := os.Getenv("TEST_DB_URL")
@@ -32,4 +55,5 @@ func NewTestApp() *App {
 func TestApp(t *testing.T) {
 	a := NewTestApp()
 	testUsersEndpoints(t, a)
+	//TODO add more here
 }
