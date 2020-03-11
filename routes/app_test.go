e457e07cf7293c41930eeeae3a8580e421aa9d5e
Prayansh Srivastava <prayansh@gmail.com>
2020-03-10 18:52:27 -0700

    write setup script

--- routes/app_test.go
+++ routes/app_test.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"database/sql"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/suite"
 	"go-api/db/postgres"
+	"go-api/utils"
 	"io"
 	"log"
 	"net/http"
@@ -37,7 +39,11 @@ type AppTestSuite struct {
 	app *App
 }
 
-func (suite *AppTestSuite) SetupTest() {
+func (suite *AppTestSuite) SetupSuite() {
+	dbUrl := os.Getenv("TEST_DB_URL")
+	if err := buildTestDB(suite.T(), dbUrl); err != nil {
+		suite.FailNow("failed to create test db")
+	}
 	suite.app = NewTestApp()
 }
 
@@ -63,4 +69,54 @@ func NewTestApp() *App {
 		store:  store,
 		gDrive: nil,
 	}
+}
+
+func buildTestDB(t *testing.T, dbUrl string) error {
+	t.Log("Beginning TestDB init...")
+	database, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		return err
+	}
+	if err = database.Ping(); err != nil {
+		return err
+	}
+	tx, err := database.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	if err = execStmts(tx, "../resources/tables.sql"); err != nil {
+		return err
+	}
+	if err = execStmts(tx, "../test-fixtures/floors.sql"); err != nil {
+		return err
+	}
+	if err = execStmts(tx, "../test-fixtures/users.sql"); err != nil {
+		return err
+	}
+	if err = execStmts(tx, "../test-fixtures/workspaces.sql"); err != nil {
+		return err
+	}
+	if err = execStmts(tx, "../test-fixtures/book_offer.sql"); err != nil {
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+	t.Log("Finishing TestDB init...")
+	return nil
+}
+
+func execStmts(tx *sql.Tx, fileName string) error {
+	statements, err := utils.ParseSqlStatements(fileName)
+	if err != nil {
+		return err
+	}
+	for _, stmt := range statements {
+		_, err = tx.Exec(stmt)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
