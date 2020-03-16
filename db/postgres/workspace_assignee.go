ea03a3ce85a76392c9bdae167d7bdc54b9e1e2ed
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-03-16 16:04:27 -0700

    Basic validation, ics wip

--- db/postgres/workspace_assignee.go
+++ db/postgres/workspace_assignee.go
@@ -2,15 +2,12 @@ package postgres
 
 import (
 	"database/sql"
-	"log"
 )
 
 func (p PostgresDBStore) IsAssigned(id string) (bool, error) {
 	sqlStatement := `SELECT workspace_id FROM workspace_assignee WHERE workspace_id=$1;`
 	var returned string
-	log.Printf("alive")
 	row := p.database.QueryRow(sqlStatement, id)
-	log.Printf("about to enter switch")
 	switch err := row.Scan(&returned); err {
 	case sql.ErrNoRows:
 		return false, nil
